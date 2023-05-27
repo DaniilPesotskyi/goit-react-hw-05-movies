74e7b930e35c0278dfe780847f1effc815f3a99a
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-05-27 12:48:15 +0300

    film card styles

---
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -0,0 +1,7 @@
+import css from './GoaBackButton.module.css';
+
+const GoBackButton = () => {
+  return <button className={css.button}>{'< GO BACK'}</button>;
+};
+
+export default GoBackButton;
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getFilmById } from '../../API/filmsAPI';
 
-// import css from './movieDetails.module.css';
+import css from './movieDetails.module.css';
+import GoBackButton from '../../components/GoBackButton/GoBackButton';
 
 const MovieDetails = () => {
   const params = useParams();
@@ -24,32 +25,51 @@ const MovieDetails = () => {
 
   return (
     <>
-    <button>{'< GO BACK'}</button>
-      <div>
+    <GoBackButton />
+      <div className={css.filmDataWrap}>
         <img
+          className={css.filmPoster}
           src={`https://image.tmdb.org/t/p/original/${
             filmData.poster_path && filmData.poster_path
           }`}
           alt="img"
         />
         <div>
-          <h1>{filmData.original_title && filmData.original_title}</h1>
-          <div>
-            <h2>User Score:</h2>
-            <p>{filmData.vote_average && filmData.vote_average}</p>
+          <h1 className={css.filmTitle}>
+            {filmData.original_title && filmData.original_title}
+          </h1>
+          <div className={css.userScoreWrap}>
+            <h2 className={css.userScoreTitle}>User Score:</h2>
+            <p className={css.userScoreValue}>
+              {filmData.vote_average && filmData.vote_average}
+            </p>
           </div>
           <div>
-            <h2>Overview</h2>
-            <p>{filmData.overview && filmData.overview}</p>
+            <h2 className={css.filmDataTitle}>Overview</h2>
+            <p className={css.filmDataValue}>
+              {filmData.overview && filmData.overview}
+            </p>
           </div>
           <div>
-            <h2>Genres</h2>
-            <ul>
+            <h2 className={css.filmDataTitle}>Genres</h2>
+            <ul className={css.filmGenresList}>
               {filmData.genres &&
-                filmData.genres.map(({ name, id }) => <li key={id}>{name}</li>)}
+                filmData.genres.map(({ name, id }) => (
+                  <li className={css.filmDataValue} key={id}>
+                    {name}
+                  </li>
+                ))}
             </ul>
           </div>
         </div>
+      </div>
+      <div className={css.additionalInfoWrap}>
+        <Link className={css.additionalInfoLink} to={`cast`}>
+          CAST
+        </Link>
+        <Link className={css.additionalInfoLink} to={`reviews`}>
+          REVIEWS
+        </Link>
       </div>
     </>
   );
